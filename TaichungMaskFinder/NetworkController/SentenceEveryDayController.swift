282ff396597f945e04a96b0aae27160821954317
MJ <terminal201467@gmail.com>
2022-04-30 19:19:12 +0800

    init the sentenceEveryDayController

---
+++ TaichungMaskFinder/NetworkController/SentenceEveryDayController.swift
@@ -0,0 +1,22 @@
+//
+//  SentenceEveryDayController.swift
+//  TaichungMaskFinder
+//
+//  Created by Jhen Mu on 2022/4/30.
+//
+
+import Alamofire
+import Foundation
+
+class SentenceEveryDayController{
+    
+    var valueChanged:(()->Void)?
+    
+    func getData(){
+        
+        
+    }
+    
+    
+    
+}
